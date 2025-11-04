ef23ec0bdb2f24ef921fc9687e5711bb603c98a0
hkym0-hub <hkym0@g.skku.edu>
2025-11-04 13:45:42 +0900

    Change title and update to Dog API viewer

--- petapp.py
+++ petapp.py
@@ -1,34 +1,15 @@
 import streamlit as st
 import requests
 
-st.title("🐾 Random Animal Viewer")
+st.title("🐶 The Dog API Viewer")
 
-# Dictionary of supported animals and their APIs
-animal_apis = {
-    "Dog": "https://dog.ceo/api/breeds/image/random",
-    "Cat": "https://api.thecatapi.com/v1/images/search",
-    "Fox": "https://randomfox.ca/floof/",
-    "Duck": "https://random-d.uk/api/v2/random"
-}
+api_key = st.secrets["api_keys"]["dogapi"]
 
-# Select the animal
-animal = st.selectbox("Choose an animal:", list(animal_apis.keys()))
+headers = {"x-api-key": api_key}
 
-if st.button("Show me the animal!"):
-    api_url = animal_apis[animal]
-    res = requests.get(api_url)
+if st.button("Show me a dog!"):
+    res = requests.get("https://api.thedogapi.com/v1/images/search", headers=headers)
     data = res.json()
-
-    # Extract image URL based on the API’s format
-    if animal == "Dog":
-        img_url = data["message"]
-    elif animal == "Cat":
-        img_url = data[0]["url"]
-    elif animal == "Fox":
-        img_url = data["image"]
-    elif animal == "Duck":
-        img_url = data["url"]
-
-    st.image(img_url, caption=f"Here's a cute {animal.lower()}!", use_container_width=True)
+    st.image(data[0]["url"], caption="Here's a cute dog!", use_container_width=True)
 else:
-    st.info("Pick an animal and click the button 🐶🐱🦊🦆")
+    st.info("Click the button to fetch a dog image 🐾")
